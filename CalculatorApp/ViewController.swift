363be1fd31fc48f69ac605e7ed143534fbe0405f
Edmond Mbadu <mbadungoma@gmail.com>
2019-10-31 10:06:30 -0400

    This is the first version of the CalculatorApp

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,11 +10,106 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number1TextField: UITextField!
+    
+    @IBOutlet weak var number2TextField: UITextField!
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func additionResult(_ sender: UIButton) {
+        
+        var num1 = 0.0
+        var num2 = 0.0
+        var result = 0.0
+        
+        if let n1 = number1TextField.text, let n2 =  number2TextField.text {
+             let trimmedN1 = n1.trimmingCharacters(in: .whitespaces)
+            let trimmedN2 = n2.trimmingCharacters(in: .whitespaces)
+            
+            if(!trimmedN1.isEmpty && !trimmedN2.isEmpty){
+                num1 = Double (trimmedN1)!
+                num2 = Double (trimmedN2)!
+                
+                result = num1 + num2
+            }
+            
+            
+        }
+        
+        resultLabel.text = "\(String(result))"
+    }
+    
+    @IBAction func substractionResult(_ sender: UIButton) {
+        var num1 = 0.0
+              var num2 = 0.0
+              var result = 0.0
+              
+              if let n1 = number1TextField.text, let n2 =  number2TextField.text {
+                   let trimmedN1 = n1.trimmingCharacters(in: .whitespaces)
+                  let trimmedN2 = n2.trimmingCharacters(in: .whitespaces)
+                  
+                  if(!trimmedN1.isEmpty && !trimmedN2.isEmpty){
+                      num1 = Double (trimmedN1)!
+                      num2 = Double (trimmedN2)!
+                      
+                      result = num1 - num2
+                  }
+                  
+                  
+              }
+              
+              resultLabel.text = "\(String(result))"
+    }
+    
+    @IBAction func multiplicationResult(_ sender: UIButton) {
+        var num1 = 0.0
+              var num2 = 0.0
+              var result = 0.0
+              
+              if let n1 = number1TextField.text, let n2 =  number2TextField.text {
+                   let trimmedN1 = n1.trimmingCharacters(in: .whitespaces)
+                  let trimmedN2 = n2.trimmingCharacters(in: .whitespaces)
+                  
+                  if(!trimmedN1.isEmpty && !trimmedN2.isEmpty){
+                      num1 = Double (trimmedN1)!
+                      num2 = Double (trimmedN2)!
+                      
+                      result = num1 * num2
+                  }
+                  
+                  
+              }
+              
+              resultLabel.text = "\(String(result))"
+    }
+    @IBAction func divisionResult(_ sender: UIButton) {
+        var num1 = 0.0
+              var num2 = 0.0
+              var result = 0.0
+              
+              if let n1 = number1TextField.text, let n2 =  number2TextField.text {
+                   let trimmedN1 = n1.trimmingCharacters(in: .whitespaces)
+                  let trimmedN2 = n2.trimmingCharacters(in: .whitespaces)
+                  
+                  if(!trimmedN1.isEmpty && !trimmedN2.isEmpty){
+                      num1 = Double (trimmedN1)!
+                      num2 = Double (trimmedN2)!
+                      
+                      result = num1 / num2
+                  }
+                  
+                  
+              }
+              
+              resultLabel.text = "\(String(result))"
+    }
 }
 
